a4f73198261a8ff46acb56693f6771e12dfacd94
kolterdyx <ciruelo@gmail.com>
2020-08-16 13:23:35 +0100

    Updated the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # @Email:  kolterdev@gmail.com
 # @Project: Pygame GUI
 # @Last modified by:   kolterdyx
-# @Last modified time: 15-Aug-2020
+# @Last modified time: 16-Aug-2020
 # @License: This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
 
 
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pgui",
-    version="0.0.4",
+    version="0.0.5",
     author="Ciro García",
     author_email="kolterdev@gmail.com",
     description="A GUI module compatible with pygame",
