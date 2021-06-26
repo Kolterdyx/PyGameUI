d6aef94b522049bf8cd4fc457451d1d28fd14eae
Kolterdyx <ciruelo@gmail.com>
2021-06-26 22:54:13 +0200

    Added .travis.yml and build.py files. Removed upload.py due to a security leak

---
+++ build.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+
+with open("version.txt", 'r') as f:
+    version = [int(x) for x in f.read().strip().split(".")]
+    version[2] += 1
+    text = "".join([str(x) + "." for x in version])[:-1]
+
+with open("version.txt", "w") as f:
+    f.write(text)
+
+os.system("python setup.py build") if sys.platform == 'win32' else os.system("python3 setup.py build")
